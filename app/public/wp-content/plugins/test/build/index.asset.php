79dc7723c1888e17fb0629f58fbf45876947bb61
Zack Bishop <zack.bishop@kanopi.com>
2024-07-16 14:50:03 -0400

    stupid gitignore

---
+++ app/public/wp-content/plugins/test/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'db874ebe48405cf176d2');
---
+++ app/public/wp-content/plugins/test/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5743e6b0eec6eae4699c');
